923bb09b094ca36d2095d191b3cbcb0878322df4
nickery <xuyilin@orientsoft.cn>
2021-03-15 17:26:38 +0800

    fix bug

--- new_word.py
+++ new_word.py
@@ -4,7 +4,7 @@ import uuid
 def new_word(db, source: str):
     word_dict_list = [x['word'] for x in db['word_stat_dict'].find({'type': 'stat'})]
     # \t、\n全部替换为空格，并将两个空格替换为1个，该替换方式无法完全避免多空格情况，所以在循环中要进行判断
-    source = source.replace('\t', ' ').replace('\n', ' ').replace('  ', '')
+    source = source.replace('\t', ' ').replace('\n', ' ').replace('\r', '').replace('  ', ' ')
     word_in_content = source.split(' ')
     word_in_content = word_in_content[2:]
     word_list = []
@@ -12,8 +12,10 @@ def new_word(db, source: str):
         if x == '':
             continue
         elif x[-1] == '།' and x[-2] != '་':
+            print(x[:-1] + '་')
             word_list.append(x[:-1] + '་')
         elif x[-1] == '།':
+            print(x[:-1])
             word_list.append(x[:-1])
         else:
             word_list.append(x)
